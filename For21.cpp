7fcf1ed9a19cc621ea39b5ad155423dc40e97b03
Yan-Zaytsev <74431649+Yan-Zaytsev@users.noreply.github.com>
2020-11-29 06:32:31 +0300

    Add files via upload

---
+++ For21.cpp
@@ -0,0 +1,25 @@
+﻿
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int n;
+    float sum = 1, fac = 1;
+    cout << "Введите N: ";
+    cin >> n;
+
+    for (int i = 1; i <= n; i++)
+    {
+        fac = fac * i;
+        sum = (sum + 1) / fac;
+    }
+    cout << "Сумма равняется: " << fac; 
+    return 0; 
+   
+}
+
+
